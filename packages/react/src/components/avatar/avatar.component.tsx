1d8822991dcc47a353c47d23dc991b65e6e8ce62
jtiagosantos <tiagosan040@gmail.com>
2023-01-11 20:13:03 -0300

    feat(react): implement avatar component

---
+++ packages/react/src/components/avatar/avatar.component.tsx
@@ -0,0 +1,17 @@
+import { User } from 'phosphor-react'
+import { AvatarContainer, AvatarImage, AvatarFallback } from './avatar.styles'
+import type { ComponentProps, FC } from 'react'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export const Avatar: FC<AvatarProps> = ({ ...props }) => {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,4 @@
 export * from './components/box.component'
 export * from './components/text.component'
 export * from './components/heading.component'
+export * from './components/avatar/avatar.component'

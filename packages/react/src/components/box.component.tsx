100c5cc9394b9cf2e876a3b991c5ea0b18f66c74
jtiagosantos <tiagosan040@gmail.com>
2023-01-11 09:08:32 -0300

    feat(docs): implement Box component and his doc

---
+++ packages/react/src/components/box.component.tsx
@@ -0,0 +1,12 @@
+import { styled } from '../styles'
+
+import type { ComponentProps } from 'react'
+
+export const Box = styled('div', {
+  padding: '$4',
+  borderRadius: '$md',
+  backgroundColor: '$gray800',
+  border: '1px solid $gray600',
+})
+
+export interface BoxProps extends ComponentProps<typeof Box> {}

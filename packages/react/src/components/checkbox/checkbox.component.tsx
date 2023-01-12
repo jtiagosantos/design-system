4d50c5d0857305c62df6bf3a81c0c01d61d051e9
jtiagosantos <tiagosan040@gmail.com>
2023-01-11 21:46:37 -0300

    feat(react): implement checkbox component

---
+++ packages/react/src/components/checkbox/checkbox.component.tsx
@@ -0,0 +1,17 @@
+import { Check } from 'phosphor-react'
+import { CheckboxContainer, CheckboxIndicator } from './checkbox.styles'
+
+import type { FC, ComponentProps } from 'react'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export const Checkbox: FC<CheckboxProps> = ({ ...props }) => {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
